6d4202d59f6366019cba990747e1254d62e9599a
entisar08 <eelmohbat@gmail.com>
2022-08-16 14:15:49 -0500

    Enum explained

---
+++ m1-generics/src/main/java/com/cydeo/Main.java
@@ -0,0 +1,36 @@
+package com.cydeo;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> items = new ArrayList<>();
+        items.add(1);
+        items.add(2);
+        items.add(3);
+        items.add(4);
+        //items.add("Apple");
+
+        printDoubled(items);
+
+
+
+
+
+
+
+
+
+    }
+
+    private static void printDoubled(ArrayList items) {
+
+        for(Object i:items){
+
+            System.out.println(( (Integer)i * 2));
+        }
+
+
+    }
+
+}
