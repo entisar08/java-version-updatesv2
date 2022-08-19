99c375ae6e8bd5ec21d33c890cb58f9e6217cf65
entisar08 <eelmohbat@gmail.com>
2022-08-19 11:10:10 -0500

    lack of functional programming in Java explained

---
+++ m3-lambda/src/main/java/com/cydeo/ArraySorting.java
@@ -0,0 +1,32 @@
+package com.cydeo;
+
+public class ArraySorting {
+    public static void main(String[] args) {
+        //methos to sort array: quick sort ,bubbleSort
+        ArraySorting as = new ArraySorting();
+        
+        //as.sort();
+
+        QuickSort qs=new QuickSort();
+        BubbleSort bs =new BubbleSort();
+
+            as.sort(qs);
+            as.sort(bs);
+            qs.sort();
+
+
+
+
+        
+    }
+
+
+
+
+
+    private void sort(Sorting sorting) {
+
+        sorting.sort();
+        //this will be excuted based on quick sort or bubble sort
+    }
+}
---
+++ m3-lambda/src/main/java/com/cydeo/Sorting.java
@@ -0,0 +1,9 @@
+package com.cydeo;
+
+public interface Sorting {
+
+    public void sort();//so we implement an interface then we create
+    // diffrent sorting classes  and let them implement the method
+
+
+}
