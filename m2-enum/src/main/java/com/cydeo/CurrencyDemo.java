aa108a70d3b4befe6db1587f1d34f1e7cbc2dda1
entisar08 <eelmohbat@gmail.com>
2022-08-18 23:25:59 -0500

    Enum COncept is explained

---
+++ m2-enum/src/main/java/com/cydeo/CurrencyDemo.java
@@ -0,0 +1,11 @@
+package com.cydeo;
+
+public class CurrencyDemo {
+
+    //this is how they used to do consttants in java before java 5
+public static  final  CurrencyDemo PENNY=new CurrencyDemo();
+public static  final  CurrencyDemo NICKLE=new CurrencyDemo();
+public static  final  CurrencyDemo DIME=new CurrencyDemo();
+public static  final  CurrencyDemo QUARTER=new CurrencyDemo();
+
+}
