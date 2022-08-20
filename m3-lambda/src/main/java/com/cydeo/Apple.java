35f54579a4f2ad82f10ee2f4ce47cc3fc6c433dc
entisar08 <eelmohbat@gmail.com>
2022-08-19 20:16:15 -0500

    PassingCodeWithBhaviorParameterization

--- m3-lambda/src/main/java/com/cydeo/Apple.java
+++ m3-lambda/src/main/java/com/cydeo/Apple.java
@@ -1,36 +1,18 @@
 package com.cydeo;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
+
 public class Apple {
     private Color color;
     private int weight;
 
 
-    public Apple(Color color, int weight) {
-        this.color = color;
-        this.weight = weight;
-    }
-
-    public Color getColor() {
-        return color;
-    }
-
-    public void setColor(Color color) {
-        this.color = color;
-    }
-
-    public int getWeight() {
-        return weight;
-    }
-
-    public void setWeight(int weight) {
-        this.weight = weight;
-    }
-
-    @Override
-    public String toString() {
-        return "Apple{" +
-                "color=" + color +
-                ", weight=" + weight +
-                '}';
-    }
 }
