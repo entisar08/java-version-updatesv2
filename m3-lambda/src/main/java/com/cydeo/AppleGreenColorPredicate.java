bfda24700cb40c01cd327fc49357144c88b6d6ee
entisar08 <eelmohbat@gmail.com>
2022-08-20 00:04:37 -0500

    Strategy Design Pattern

--- m3-lambda/src/main/java/com/cydeo/AppleGreenColorPredicate.java
+++ m3-lambda/src/main/java/com/cydeo/AppleGreenColorPredicate.java
@@ -3,6 +3,6 @@ package com.cydeo;
 public class AppleGreenColorPredicate implements ApplePredicate{
     @Override
     public boolean test(Apple apple) {
-        return apple.getColor().equals(Color.GREEn);
+        return apple.getColor().equals(Color.GREEN);
     }
 }
